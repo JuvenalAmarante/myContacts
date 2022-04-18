1cc1dac812420ca5510bfe46460955d64622236c
Juvenal Amarante <juvenalamarante13@gmail.com>
2022-04-18 13:51:45 -0300

    Adicionando arquivo de rotas e Controller de Contatos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-const express = require("express");
+const express = require('express');
+const routes = require('./routes');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello");
-});
+app.use(routes);
 
-app.listen(3000, () =>
-  console.log("Servidor Iniciado em http://localhost:3000")
-);
+app.listen(3000, () => console.log('Servidor Iniciado em http://localhost:3000'));
